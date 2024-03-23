70377ddd06de91e568395984c23faaaaa90f4db5
Bogdan Kovtun <psirex48@gmail.com>
2024-03-23 04:07:39 +0400

    The first draft of the Escrow with changed mechanics

---
+++ contracts/interfaces/IWstETH.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.23;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IWstETH is IERC20 {
+    function wrap(uint256 stETHAmount) external returns (uint256);
+
+    function unwrap(uint256 wstETHAmount) external returns (uint256);
+}
