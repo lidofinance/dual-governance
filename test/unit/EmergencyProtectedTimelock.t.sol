976a1d7c1a3ca4de378f4b830c74cabaeb72445e
Roman Kolpakov <rkolpakov94@gmail.com>
2024-04-22 21:16:26 +0700

    feat: transferExecutorOwnership tested

--- test/unit/EmergencyProtectedTimelock.t.sol
+++ test/unit/EmergencyProtectedTimelock.t.sol
@@ -2,7 +2,9 @@
 pragma solidity 0.8.23;
 
 import {Test, Vm} from "forge-std/Test.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+import {Executor} from "contracts/Executor.sol";
 import {EmergencyProtectedTimelock} from "contracts/EmergencyProtectedTimelock.sol";
 import {IConfiguration, Configuration} from "contracts/Configuration.sol";
 import {ConfigurationProvider} from "contracts/ConfigurationProvider.sol";
@@ -15,37 +17,43 @@ contract EmergencyProtectedTimelockUnitTests is Test {
 
     address private _emergencyGovernance = makeAddr("emergencyGovernance");
     address private _dualGovernance = makeAddr("dualGovernance");
-    address private _executor = makeAddr("executor");
+    address private _adminExecutor = makeAddr("executor");
 
     function setUp() external {
-        _config = new Configuration(_executor, _emergencyGovernance, new address[](0));
+        _config = new Configuration(_adminExecutor, _emergencyGovernance, new address[](0));
         _timelock = new EmergencyProtectedTimelock(address(_config));
     }
 
-    function test_admin_executor_can_set_governance() external {
-        vm.recordLogs();
+    // EmergencyProtectedTimelock.setGovernance()
 
+    function test_admin_executor_can_set_governance() external {
         assertEq(_timelock.getGovernance(), address(0));
 
-        vm.prank(_executor);
+        vm.recordLogs();
+
+        vm.prank(_adminExecutor);
         _timelock.setGovernance(_dualGovernance);
 
         assertEq(_timelock.getGovernance(), _dualGovernance);
 
         Vm.Log[] memory entries = vm.getRecordedLogs();
         assertEq(entries.length, 1);
+        assertEq(entries[0].emitter, address(_timelock));
         assertEq(entries[0].topics[0], EmergencyProtectedTimelock.GovernanceSet.selector);
+        // There is no topic with value in the event (foundry bug??)
+        assertEq(entries[0].topics.length, 1);
+
         assertEq(abi.decode(entries[0].data, (address)), _dualGovernance);
     }
 
     function test_cannot_set_governance_to_zero() external {
-        vm.prank(_executor);
+        vm.prank(_adminExecutor);
         vm.expectRevert(abi.encodeWithSelector(EmergencyProtectedTimelock.InvalidGovernance.selector, address(0)));
         _timelock.setGovernance(address(0));
     }
 
     function test_cannot_set_governance_to_the_same_address() external {
-        vm.startPrank(_executor);
+        vm.startPrank(_adminExecutor);
 
         _timelock.setGovernance(_dualGovernance);
         assertEq(_timelock.getGovernance(), _dualGovernance);
@@ -58,10 +66,46 @@ contract EmergencyProtectedTimelockUnitTests is Test {
     }
 
     function testFuzz_stranger_cannot_set_governance(address stranger) external {
-        vm.assume(stranger != _executor);
+        vm.assume(stranger != _adminExecutor);
 
         vm.prank(stranger);
         vm.expectRevert(abi.encodeWithSelector(ConfigurationProvider.NotAdminExecutor.selector, stranger));
         _timelock.setGovernance(makeAddr("newGovernance"));
+    }
+
+    // EmergencyProtectedTimelock.transferExecutorOwnership()
+
+    function testFuzz_admin_executor_can_transfer_executor_ownership(address newOwner) external {
+        vm.assume(newOwner != _adminExecutor);
+        vm.assume(newOwner != address(0));
+
+        Executor executor = new Executor(address(_timelock));
+
+        assertEq(executor.owner(), address(_timelock));
+
+        vm.recordLogs();
+
+        vm.prank(_adminExecutor);
+        _timelock.transferExecutorOwnership(address(executor), newOwner);
+
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        assertEq(entries.length, 1);
+        assertEq(entries[0].emitter, address(executor));
+        assertEq(entries[0].topics[0], Ownable.OwnershipTransferred.selector);
+        assertEq(entries[0].topics[1], bytes32(uint256(uint160(address(_timelock)))));
+        assertEq(entries[0].topics[2], bytes32(uint256(uint160(newOwner))));
+
+        // There is no data in the event (foundry bug??)
+        assertEq(bytes32(entries[0].data), bytes32(uint256(0)));
+
+        assertEq(executor.owner(), newOwner);
+    }
+
+    function test_stranger_cannot_transfer_executor_ownership(address stranger) external {
+        vm.assume(stranger != _adminExecutor);
+
+        vm.prank(stranger);
+        vm.expectRevert(abi.encodeWithSelector(ConfigurationProvider.NotAdminExecutor.selector, stranger));
+        _timelock.transferExecutorOwnership(_adminExecutor, makeAddr("newOwner"));
     }
 }
