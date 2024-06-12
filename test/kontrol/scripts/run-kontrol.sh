fb685aa6839fcc304ebc1a8739da0d82e8c6ddc8
lucasmt <lmartinellitabajara@gmail.com>
2024-06-12 13:37:22 -0500

    Add separate profile for kontrol proofs

--- test/kontrol/scripts/run-kontrol.sh
+++ test/kontrol/scripts/run-kontrol.sh
@@ -5,9 +5,9 @@ SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # shellcheck source=/dev/null
 source "$SCRIPT_HOME/common.sh"
 export RUN_KONTROL=true
-CUSTOM_FOUNDRY_PROFILE=default
+CUSTOM_FOUNDRY_PROFILE=kprove
 export FOUNDRY_PROFILE=$CUSTOM_FOUNDRY_PROFILE
-export OUT_DIR=out # out dir of $FOUNDRY_PROFILE
+export OUT_DIR=kout # out dir of $FOUNDRY_PROFILE
 parse_args "$@"
 
 #############
