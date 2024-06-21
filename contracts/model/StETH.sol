4eff4c7a7949ae9fc70dcbd895f2605a1b495d06
lucasmt <lmartinellitabajara@gmail.com>
2024-06-21 13:17:49 -0500

    Adjust StETH model to be closer to original StETH contract

--- contracts/model/StETH.sol
+++ contracts/model/StETH.sol
@@ -1,10 +1,12 @@
 pragma solidity 0.8.23;
 
 contract StETH {
-    uint256 public totalPooledEther;
-    uint256 public totalShares;
-    mapping(address => uint256) public shares;
-    mapping(address => mapping(address => uint256)) public allowances;
+    uint256 private totalPooledEther;
+    uint256 private totalShares;
+    mapping(address => uint256) private shares;
+    mapping(address => mapping(address => uint256)) private allowances;
+
+    uint256 internal constant INFINITE_ALLOWANCE = type(uint256).max;
 
     function setTotalPooledEther(uint256 _value) external {
         totalPooledEther = _value;
@@ -18,8 +20,60 @@ contract StETH {
         shares[_account] = _value;
     }
 
+    function setAllowances(address _owner, address _spender, uint256 _amount) external {
+        allowances[_owner][_spender] = _amount;
+    }
+
     function totalSupply() external view returns (uint256) {
         return totalPooledEther;
+    }
+
+    function getTotalPooledEther() external view returns (uint256) {
+        return totalPooledEther;
+    }
+
+    function balanceOf(address _account) external view returns (uint256) {
+        return getPooledEthByShares(shares[_account]);
+    }
+
+    function transfer(address _recipient, uint256 _amount) external returns (bool) {
+        _transfer(msg.sender, _recipient, _amount);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) external view returns (uint256) {
+        return allowances[_owner][_spender];
+    }
+
+    function approve(address _spender, uint256 _amount) external returns (bool) {
+        _approve(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool) {
+        _spendAllowance(_sender, msg.sender, _amount);
+        _transfer(_sender, _recipient, _amount);
+        return true;
+    }
+
+    function increaseAllowance(address _spender, uint256 _addedValue) external returns (bool) {
+        _approve(msg.sender, _spender, allowances[msg.sender][_spender] + _addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address _spender, uint256 _subtractedValue) external returns (bool) {
+        uint256 currentAllowance = allowances[msg.sender][_spender];
+        require(currentAllowance >= _subtractedValue, "ALLOWANCE_BELOW_ZERO");
+        _approve(msg.sender, _spender, currentAllowance - _subtractedValue);
+        return true;
+    }
+
+    function getTotalShares() external view returns (uint256) {
+        return totalShares;
+    }
+
+    function sharesOf(address _account) external view returns (uint256) {
+        return shares[_account];
     }
 
     function getSharesByPooledEth(uint256 _ethAmount) public view returns (uint256) {
@@ -30,20 +84,41 @@ contract StETH {
         return _sharesAmount * totalPooledEther / totalShares;
     }
 
-    function _sharesOf(address _account) internal view returns (uint256) {
-        return shares[_account];
+    function transferShares(address _recipient, uint256 _sharesAmount) external returns (uint256) {
+        _transferShares(msg.sender, _recipient, _sharesAmount);
+        uint256 tokensAmount = getPooledEthByShares(_sharesAmount);
+        return tokensAmount;
     }
 
-    function sharesOf(address _account) external view returns (uint256) {
-        return _sharesOf(_account);
+    function transferSharesFrom(
+        address _sender,
+        address _recipient,
+        uint256 _sharesAmount
+    ) external returns (uint256) {
+        uint256 tokensAmount = getPooledEthByShares(_sharesAmount);
+        _spendAllowance(_sender, msg.sender, tokensAmount);
+        _transferShares(_sender, _recipient, _sharesAmount);
+        return tokensAmount;
     }
 
-    function allowance(address _owner, address _spender) external view returns (uint256) {
-        return allowances[_owner][_spender];
+    function _transfer(address _sender, address _recipient, uint256 _amount) internal {
+        uint256 _sharesToTransfer = getSharesByPooledEth(_amount);
+        _transferShares(_sender, _recipient, _sharesToTransfer);
     }
 
-    function balanceOf(address _account) external view returns (uint256) {
-        return getPooledEthByShares(_sharesOf(_account));
+    function _approve(address _owner, address _spender, uint256 _amount) internal {
+        require(_owner != address(0), "APPROVE_FROM_ZERO_ADDR");
+        require(_spender != address(0), "APPROVE_TO_ZERO_ADDR");
+
+        allowances[_owner][_spender] = _amount;
+    }
+
+    function _spendAllowance(address _owner, address _spender, uint256 _amount) internal {
+        uint256 currentAllowance = allowances[_owner][_spender];
+        if (currentAllowance != INFINITE_ALLOWANCE) {
+            require(currentAllowance >= _amount, "ALLOWANCE_EXCEEDED");
+            _approve(_owner, _spender, currentAllowance - _amount);
+        }
     }
 
     function _transferShares(address _sender, address _recipient, uint256 _sharesAmount) internal {
@@ -56,40 +131,5 @@ contract StETH {
 
         shares[_sender] = currentSenderShares - _sharesAmount;
         shares[_recipient] = shares[_recipient] + _sharesAmount;
-    }
-
-    function transfer(address _recipient, uint256 _amount) external returns (bool) {
-        uint256 sharesAmount = getSharesByPooledEth(_amount);
-        _transferShares(msg.sender, _recipient, sharesAmount);
-        return true;
-    }
-
-    function transferShares(address _recipient, uint256 _sharesAmount) external returns (bool) {
-        _transferShares(msg.sender, _recipient, _sharesAmount);
-        return true;
-    }
-
-    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool) {
-        uint256 currentAllowance = allowances[_sender][msg.sender];
-        require(currentAllowance >= _amount, "ALLOWANCE_EXCEEDED");
-
-        uint256 sharesAmount = getSharesByPooledEth(_amount);
-
-        _transferShares(_sender, _recipient, sharesAmount);
-        allowances[_sender][msg.sender] = currentAllowance - _amount;
-
-        return true;
-    }
-
-    function _approve(address _owner, address _spender, uint256 _amount) internal {
-        require(_owner != address(0), "APPROVE_FROM_ZERO_ADDR");
-        require(_spender != address(0), "APPROVE_TO_ZERO_ADDR");
-
-        allowances[_owner][_spender] = _amount;
-    }
-
-    function approve(address _spender, uint256 _amount) external returns (bool) {
-        _approve(msg.sender, _spender, _amount);
-        return true;
     }
 }
