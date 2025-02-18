dc20324a0ce17ef044cec1e094597291541544fa
Aleksandr Tarelkin <tarelkinaa@bk.ru>
2025-02-18 13:39:07 +0300

    WIP script wrapper with .env

---
+++ run-script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+export $(grep -v '^#' .env | xargs)
+
+SCRIPT_PATH="$1"
+shift
+
+CONTRACT_NAME=$(basename "$SCRIPT_PATH" .s.sol)
+
+# Run Foundry script
+forge script $SCRIPT_PATH:$CONTRACT_NAME --rpc-url $RPC_URL --account $DEPLOYER_ACCOUNT --etherscan-api-key $ETHERSCAN_MAINNET_KEY --force $@
