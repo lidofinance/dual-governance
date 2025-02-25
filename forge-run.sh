a270faf33ed850a09fc1bec226d2595d0da63b2b
Aleksandr Tarelkin <tarelkinaa@bk.ru>
2025-02-25 14:10:56 +0300

    optional fields for forge run script

--- forge-run.sh
+++ forge-run.sh
@@ -8,11 +8,23 @@ shift
 
 CONTRACT_NAME=$(basename "$SCRIPT_PATH" .s.sol)
 
+args=(
+  "$SCRIPT_PATH:$CONTRACT_NAME"
+  "--rpc-url" "$RPC_URL"
+  "--etherscan-api-key" "$ETHERSCAN_API_KEY"
+  "--force"
+)
+
+if [[ -n "$DEPLOYER_ACCOUNT" ]]; then
+  args+=( "--account" "$DEPLOYER_ACCOUNT" )
+fi
+
+if [[ -n "$DEPLOYER_ADDRESS" ]]; then
+  args+=( "--sender" "$DEPLOYER_ADDRESS" )
+fi
+
+args+=( "$@" )
+
 # Run Foundry script
-forge \
-    script $SCRIPT_PATH:$CONTRACT_NAME \
-    --rpc-url $RPC_URL \
-    --account $DEPLOYER_ACCOUNT \
-    --etherscan-api-key $ETHERSCAN_API_KEY \
-    --force \
-    $@
+
+forge script "${args[@]}"
