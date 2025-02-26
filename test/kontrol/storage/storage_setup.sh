6059992275597f5be5fa8c30310645ad14853a0d
lucasmt <lmartinellitabajara@gmail.com>
2025-02-26 11:11:36 -0300

    Add `--json` option to `forge inspect storage` in `storage_setup.sh` script
    This makes it compatible with more recent versions of Forge, where pretty printing the storage layout is the default

--- test/kontrol/storage/storage_setup.sh
+++ test/kontrol/storage/storage_setup.sh
@@ -1,8 +1,8 @@
-forge inspect DualGovernance storage > test/kontrol/storage/DualGovernance.json
-forge inspect Escrow storage > test/kontrol/storage/Escrow.json
-forge inspect StETHModel storage > test/kontrol/storage/StETHModel.json
-forge inspect WithdrawalQueueModel storage > test/kontrol/storage/WithdrawalQueueModel.json
-forge inspect EmergencyProtectedTimelock storage > test/kontrol/storage/EmergencyProtectedTimelock.json
+forge inspect DualGovernance storage --json > test/kontrol/storage/DualGovernance.json
+forge inspect Escrow storage --json > test/kontrol/storage/Escrow.json
+forge inspect StETHModel storage --json > test/kontrol/storage/StETHModel.json
+forge inspect WithdrawalQueueModel storage --json > test/kontrol/storage/WithdrawalQueueModel.json
+forge inspect EmergencyProtectedTimelock storage --json > test/kontrol/storage/EmergencyProtectedTimelock.json
 
 python3 test/kontrol/storage/storage_setup.py test/kontrol/storage/DualGovernance.json 0.8.26 DualGovernance > test/kontrol/storage/DualGovernanceStorageConstants.sol
 python3 test/kontrol/storage/storage_setup.py test/kontrol/storage/Escrow.json 0.8.26 Escrow > test/kontrol/storage/EscrowStorageConstants.sol
